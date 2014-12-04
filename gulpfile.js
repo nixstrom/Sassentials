bf892e082e54850a8360e2e7564a124132f0039e
Nikki Strømsnes <nikkistromsnes@gmail.com>
2014-12-04 22:17:57 +0100

    Purge Compass from Gulp build! Begone, ye daemon!
    It has been replaced by ruby-sass and autoprefixer. At this time, there
    is a slight bug in ruby-sass, meaning it doesn’t allow autoprefixer and
    source maps to play well together.
    
    If this is not fixed, I will contemplate replacing gulp-ruby-sass  with
    gulp-sass and gulp-sourcemaps.
    
    gulp-ruby-sass was chosen, simply because it looks a bit neater, but I
    can go either way.

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,23 @@
 var gulp    = require('gulp');
+
+// For CSS
+var sass       = require('gulp-ruby-sass');
+var autoprefix = require('gulp-autoprefixer') ;
+
+// For JS
 var uglify  = require('gulp-uglify');
-var concat  = require('gulp-concat');
 var jshint  = require('gulp-jshint');
 var stylish = require('jshint-stylish');
-var compass = require('gulp-compass');
-var notify  = require('gulp-notify');
 
-var browserify = require('gulp-browserify');
-var rename = require('gulp-rename');
+// For I/O
+var concat  = require('gulp-concat');
+var notify  = require('gulp-notify');
 
 
 var paths = {
     scripts: {
         src:  'assets/js/*.js',
         dest: 'public/javascripts'
-    },
-    coffee: {
-      src:  'assets/js/coffee/**/*.coffee',
-      dest: 'assets/js'
     },
     styles: {
         src:  'assets/sass/*.scss',
@@ -29,42 +29,29 @@ var paths = {
 var development = true;
 
 // Defaults are for production environment (development = false)
-var compassDefaults = {
-  css: paths.styles.dest,
-  sass: 'assets/sass',
-  image: 'assets/images',
+var sassDefaults = {
   require: ['susy', 'breakpoint'],
-  style: 'compressed'
+  sourcemap: false,
+  'sourcemap=none': true,  
+  style: "nested"
 }
 
 gulp.task('styles', function () {
   if (development === true) {
     gulp.src('assets/sass/main.scss')
-      .pipe(compass( compassDefaults ))
+      .pipe(sass( sassDefaults ))
+      .pipe(autoprefix('last 2 version'))
+      .pipe(gulp.dest(paths.styles.dest))
       .pipe(notify({ message: 'Styles (DEV) task complete. \n Saved to ' + paths.styles.dest }));
-
   } else {
     // Override defaults, so output is trimmed down
-    compassDefaults['sourcemap'] = true;
-    compassDefaults['style'] = 'nested';
-    compassDefaults['compass'] = false;
+		sassDefaults['style'] = "compressed";
 
     gulp.src('temp/css/main.scss')
-      .pipe(compass( compassDefaults ))
+      .pipe(sass( sassDefaults ))
       .pipe(notify({ message: 'Styles task complete. \n Saved to ' + paths.styles.dest }));
   }
 });
-
-gulp.task('coffee', function() {
-  gulp.src('assets/js/coffee/app.coffee', { read: false })
-    .pipe(browserify({
-      transform: ['coffeeify'],
-      extensions: ['.coffee']
-    }))
-    .pipe(rename('app.js'))
-    .pipe(gulp.dest('./public/javascripts'))
-});
-
 
 // Scripts task
 gulp.task('scripts', function () {
@@ -86,7 +73,6 @@ gulp.task('scripts', function () {
 // Watches files for modifications and reloads
 gulp.task('watch', function() {
   gulp.watch('assets/sass/**/*.scss', ['styles']);
-  gulp.watch(paths.coffee.src, ['coffee', 'scripts']);
   gulp.watch(paths.scripts.src, ['scripts']);
 });
 
